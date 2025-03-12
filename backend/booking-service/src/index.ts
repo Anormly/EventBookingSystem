23529348f0b82237893b7c17b4514d109afae4d4
Anormly <danikvoron1@gmail.com>
2025-03-12 08:56:04 +0300

    Подключен маршрут для документации, исправлены зависимости

--- backend/booking-service/src/index.ts
+++ backend/booking-service/src/index.ts
@@ -4,6 +4,8 @@ import cors from "cors";
 import { AppDataSource } from "./config/db";
 import eventRoutes from "./routes/event.routes";
 import bookingRoutes from "./routes/booking.routes";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./config/swagger.json";
 
 dotenv.config();
 
@@ -12,6 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // Роуты
 app.use("/api/events", eventRoutes);
 app.use("/api/bookings", bookingRoutes);
